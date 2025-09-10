0f288a4955a750b22ccfdc827593a61235823485
drujban <drujbanjo@gmail.com>
2025-09-10 19:52:16 +0500

    fix[cors]: enabled cors

--- apps/backend/api/index.ts
+++ apps/backend/api/index.ts
@@ -7,9 +7,14 @@ let cachedHandler: (req: Request, res: Response) => void
 export default async function handler(req: Request, res: Response) {
 	if (!cachedHandler) {
 		const app = await NestFactory.create(AppModule)
+
+		app.enableCors()
+
 		await app.init()
+
 		const instance = app.getHttpAdapter().getInstance()
 		cachedHandler = instance as unknown as (req: Request, res: Response) => void
 	}
+
 	return cachedHandler(req, res)
 }
