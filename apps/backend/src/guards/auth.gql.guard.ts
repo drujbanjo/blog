ecb0bf413daf284108bc9e2a6bb7d6e09b806ffa
drujban <drujbanjo@gmail.com>
2025-09-22 09:28:01 +0500

    fix: fixed guard. fromt cookies to headers

--- apps/backend/src/guards/auth.gql.guard.ts
+++ apps/backend/src/guards/auth.gql.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from "@nestjs/common"
 import { JwtService } from "@nestjs/jwt"
 import { GqlExecutionContext } from "@nestjs/graphql"
-
 import { Request } from "express"
 
 @Injectable()
@@ -11,8 +10,8 @@ export class GqlJwtAuthGuard implements CanActivate {
 	canActivate(context: ExecutionContext): boolean {
 		const ctx = GqlExecutionContext.create(context)
 		const req = ctx.getContext<{ req: Request }>().req
-		const token = req.cookies?.token // ключевой момент
 
+		const token = req.headers.token as string | undefined
 		if (!token) throw new UnauthorizedException("Token not found")
 
 		try {
