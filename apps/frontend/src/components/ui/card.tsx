77fb53d771552d873974640415ce3b93aa379823
drujban <drujbanjo@gmail.com>
2026-01-06 12:31:26 +0500

    fix: card title

--- apps/frontend/src/components/ui/card.tsx
+++ apps/frontend/src/components/ui/card.tsx
@@ -23,7 +23,7 @@ function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
 }
 
 function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
-	return <div data-slot="card-title" className={cn("text-xl leading-none font-semibold", className)} {...props} />
+	return <div data-slot="card-title" className={cn("text-lg leading-none font-semibold", className)} {...props} />
 }
 
 function CardDescription({ className, ...props }: React.ComponentProps<"div">) {
